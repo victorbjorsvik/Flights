d127edd8dcef2f965a0a28b1c2afd03132adbda1
victorbjorsvik <victorbjosvik@hotmail.com>
2024-02-26 20:06:27 +0000

    added flights.py

---
+++ flights.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Flights():
+    def __init__(self):
+        if not os.path.exists("Downloads"):
+            os.makedirs("Downloads")
+
+
+

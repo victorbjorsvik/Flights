f8eb4a8c35d53a1c0d72ac2b28d9d8e505b1b26b
victorbjorsvik <victorbjorsvik@hotmail.com>
2024-03-04 13:36:26 +0000

    added prototype_1

--- Functions/flights.py
+++ Functions/flights.py
@@ -1,12 +1,36 @@
+"""
+Module for handling flightdata
+"""
+
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import requests
 
 class Flights():
     def __init__(self):
-        if not os.path.exists("Downloads"):
-            os.makedirs("Downloads")
+        if not os.path.exists("../Downloads"):
+            os.makedirs("../Downloads")
+
+        if not os.path.exists("../Downloads/flight_data"):
+            url = "https://gitlab.com/adpro1/adpro2024/-/raw/main/Files/flight_data.zip?inline=false"
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open("../Downloads/flight_data", "wb") as file:
+                    file.write(response.content)
+                print("Files downloaded")
+            else:
+                print("Could not import data")
+        else:
+            print("Data already imported")
+
+flights = Flights()
+
+
+
+
+
 
 
 
